bc46318011ee0afcf864b9dff81cdcb63ed2bb9b
aniketmethi@gmail.com <aniketmethi@gmail.com>
2022-09-19 21:00:12 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './About'
+import Contact from './Contact'
+import Home from './Home'
+import NavBar from './NavBar'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<NavBar></NavBar>}>
+                      <Route index path='/' element={<Home></Home>}></Route>
+                    <Route path='/about' element={<About></About>}></Route>
+                    <Route path='/contact' element={<Contact></Contact>}></Route>
+                    <Route path='/*' element={<div><h1>Error 404 Not Found......</h1></div>}></Route>
+                    </Route>
+                </Routes>
+        </BrowserRouter>
     </div>
-  );
+  )
 }
-
-export default App;
